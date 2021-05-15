c5c1fe922a3c4fab514ea7a7ea65e3dc3df24aef
TouhidTushar <mohd.touhedul.alam@g.bracu.ac.bd>
2021-05-15 18:52:29 +0600

    update

---
+++ main.js
@@ -0,0 +1,21 @@
+//ripple buttons
+var val = 0;
+window.addEventListener("scroll", function () {
+  val = window.scrollY;
+});
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY + val - e.target.offsetTop;
+
+    let ripples = document.createElement("p");
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px";
+    this.appendChild(ripples);
+    setTimeout(() => {
+      ripples.remove();
+    }, 1000);
+  });
+});
